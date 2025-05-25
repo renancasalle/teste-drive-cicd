f25828230a9c2859da2d81f4266427b5c1bd4c3d
renancasalle <renan.goncalves@sptech.school>
2025-05-25 18:29:58 -0300

    fix global execption handler params

--- src/main/java/com/example/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/api/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.api.exception;
 
+import com.example.api.exception.product.ProductFieldsNullException;
+import com.example.api.exception.product.ProductNameAlreadyExistsException;
+import com.example.api.exception.product.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -19,38 +22,37 @@ public class GlobalExceptionHandler {
             errors.put(error.getField(), error.getDefaultMessage());
         });
 
-        return buildExceptionResponse(null, 400, "Validation Error");
+        return buildExceptionResponse(HttpStatus.BAD_REQUEST, "Validation Error");
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
-        return buildExceptionResponse(null, 400, "Malformed JSON request");
+        return buildExceptionResponse(HttpStatus.BAD_REQUEST, "Malformed JSON request");
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleGeneralException(Exception ex) {
-        return buildExceptionResponse(null, 500, "Internal Server Error");
+        return buildExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
     }
 
     @ExceptionHandler(ProductNameAlreadyExistsException.class)
     public ResponseEntity<ExceptionResponse> handleProductNameAlreadyExists(ProductNameAlreadyExistsException ex) {
-        return buildExceptionResponse(null, 400, ex.getMessage());
+        return buildExceptionResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleProductNotFound(ProductNotFoundException ex) {
-        return buildExceptionResponse(null, 404, ex.getMessage());
+        return buildExceptionResponse(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
     @ExceptionHandler(ProductFieldsNullException.class)
     public ResponseEntity<ExceptionResponse> handleProductFieldsNull(ProductFieldsNullException ex) {
-        return buildExceptionResponse(null, 400, ex.getMessage());
+        return buildExceptionResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
-    private ResponseEntity<ExceptionResponse> buildExceptionResponse(HttpStatus staus, String detail) {
+    private ResponseEntity<ExceptionResponse> buildExceptionResponse(HttpStatus status, String detail) {
         return ResponseEntity
                 .status(status)
                 .body(new ExceptionResponse(status.value(), detail));
     }
-
 }
