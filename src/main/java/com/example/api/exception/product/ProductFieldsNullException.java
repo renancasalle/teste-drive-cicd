e2946a05ba2dd50e3c207bbf62930da7050e7dc4
renancasalle <renan.goncalves@sptech.school>
2025-05-25 18:29:13 -0300

    feat product fields null exception

---
+++ src/main/java/com/example/api/exception/product/ProductFieldsNullException.java
@@ -0,0 +1,7 @@
+package com.example.api.exception.product;
+
+public class ProductFieldsNullException extends RuntimeException {
+    public ProductFieldsNullException(String message) {
+        super(message);
+    }
+}
